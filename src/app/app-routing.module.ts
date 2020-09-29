23c9f22da977f03e284780443ad46263f8581b97
santosEsp <johndoe@example.com>
2020-09-29 12:32:31 -0500

    Version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import {LoginComponent} from './components/Login/login.component';
+import {ContactosComponent} from './components/pages/contactos/contactos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'Login', component: LoginComponent},
+  {path: 'Contactos', component: ContactosComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'Login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/Login/login.component';
+import { EmpresaComponent } from './components/pages/empresa/empresa.component';
+
+import { ContactosComponent } from './components/pages/contactos/contactos.component';
+import { PipelineComponent } from './components/pages/pipeline/pipeline.component';
+import { NavbarComponent } from './components/pages/navbar/navbar.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmpresaComponent,
+    ContactosComponent,
+    PipelineComponent,
+    NavbarComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
